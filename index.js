b162b2b1b7aebb153f69930d060de7f01c8ae12c
monstermiles <miles.emmitt.moss@gmail.com>
2022-02-11 16:49:39 -0600

    got a simple get route working

--- index.js
+++ index.js
@@ -1,56 +1,96 @@
 const express = require('express');
-// Run npm install mongodb and require mongodb and MongoClient class
-const mongodb = require('mongodb').MongoClient;
+const db = require('./config/connection');
 
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const port = 3001;
 
-// Connection string to local instance of MongoDB including database name
-const connectionStringURI = `mongodb://localhost:27017/socialnetworkDB`;
-
-// Declare a variable to hold the connection
-let db;
-
-// Creates a connection to a MongoDB instance and returns the reference to the database
-mongodb.connect(
-  // Defines connection between app and MongoDB instance
-  connectionStringURI,
-  // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    // Use client.db() constructor to add new db instance
-    db = client.db();
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`);
-    });
-  }
-);
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-
-
-app.post('/create', (req, res) => {
-  // Use db connection to add a document
-  db.collection('users').insertOne(
-    { name: req.body.name, breed: req.body.breed },
-    (err, results) => {
-      if (err) throw err;
-      res.json(results);
-    }
-  );
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
 
-app.get('/read', (req, res) => {
-  // Use db connection to find all documents in collection
-  db.collection('users')
-    .find()
-    .toArray((err, results) => {
-      if (err) throw err;
-      res.send(results);
+
+
+// Require model
+const { User } = require('./models');
+
+
+
+app.get('/users', (req, res) => {
+    // Using model in route to find all documents that are instances of that model
+    User.find({}, (err, result) => {
+      if (err) {
+        res.status(500).send({ message: 'Internal Server Error' });
+      } else {
+        res.status(200).json(result);
+      }
     });
-});
+  });
+
+
+
+
+  
+
+// const express = require('express');
+// // Run npm install mongodb and require mongodb and MongoClient class
+// const mongodb = require('mongodb').MongoClient;
+
+// const app = express();
+// const port = 3001;
+
+// // Connection string to local instance of MongoDB including database name
+// const connectionStringURI = `mongodb://localhost:27017/socialnetworkDB`;
+
+// // Declare a variable to hold the connection
+// let db;
+
+// // Creates a connection to a MongoDB instance and returns the reference to the database
+// mongodb.connect(
+//   // Defines connection between app and MongoDB instance
+//   connectionStringURI,
+//   // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, client) => {
+//     // Use client.db() constructor to add new db instance
+//     db = client.db();
+//     app.listen(port, () => {
+//       console.log(`Example app listening at http://localhost:${port}`);
+//     });
+//   }
+// );
+
+// app.use(express.json());
+
+
+
+
+
+// app.post('/create', (req, res) => {
+//   // Use db connection to add a document
+//   db.collection('users').insertOne(
+//     { name: req.body.name, breed: req.body.breed },
+//     (err, results) => {
+//       if (err) throw err;
+//       res.json(results);
+//     }
+//   );
+// });
+
+// app.get('/read', (req, res) => {
+//   // Use db connection to find all documents in collection
+//   db.collection('users')
+//     .find()
+//     .toArray((err, results) => {
+//       if (err) throw err;
+//       res.send(results);
+//     });
+// });
+
 
 
--- models/User.js
+++ models/User.js
@@ -1,3 +1,50 @@
+const {Schema, model} = require('mongoose');
+// const mongoose = require('mongoose')
+
+// * `username`
+//   * String
+//   * Unique
+//   * Required
+//   * Trimmed
+
+// * `email`
+//   * String
+//   * Required
+//   * Unique
+//   * Must match a valid email address (look into Mongoose's matching validation)
+
+// * `thoughts`
+//   * Array of `_id` values referencing the `Thought` model
+
+// * `friends`
+//   * Array of `_id` values referencing the `User` model (self-reference)
+
+  
+const userSchema = new Schema({
+    username: {
+        type: String, 
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String, 
+        unique: true,
+        required: true,
+        // match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`,
+    },
+    // thoughts: [
+    //     {type: Schema.Types.ObjectId,
+    //      ref: 'Thoughts' 
+    //     }
+    // ],
+    // friends: [
+    //     {type: Schema.Types.ObjectId,
+    //     ref: 'User'}
+    // ]
+});
+
+const User = model('user', userSchema)
 
 
-
+module.exports = User;
